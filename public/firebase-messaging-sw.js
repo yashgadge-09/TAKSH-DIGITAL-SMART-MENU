67201b86fc4893efe910f9c6da9b3096cfc589a8
Yash Gadge <yaashgadge@gmail.com>
2026-05-17 01:36:36 +0530

    updateSW

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,7 +1,6 @@
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
-// Paste your Firebase config values here
 firebase.initializeApp({
   apiKey: "AIzaSyAdlNzP5mFxBah5dFc6obMKnnsZS2ZSMiE",
   authDomain: "tastefynotification.firebaseapp.com",
@@ -13,22 +12,20 @@ firebase.initializeApp({
 
 const messaging = firebase.messaging();
 
-// This handles background notifications
 messaging.onBackgroundMessage(function (payload) {
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
-    icon: '/icon-192x192.png',
+    icon: '/apple-icon.png',
+    badge: '/apple-icon.png',
     data: payload.data
   };
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
 
-// Handle notification click
 self.addEventListener('notificationclick', function (event) {
   event.notification.close();
-
   if (event.notification.data && event.notification.data.url) {
     event.waitUntil(
       clients.openWindow(event.notification.data.url)
